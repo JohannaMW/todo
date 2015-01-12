6a20b82d2c00dd9fc65a0c8d6460a9d8da73ccf2
Johanna <jweinsziehr@gmx.de>
2015-01-12 17:08:24 +0100

    editing api

--- todo_list/api/serializers.py
+++ todo_list/api/serializers.py
@@ -2,8 +2,15 @@ from rest_framework import serializers
 from todo_list.models import User, Task
 
 class UserSerializer(serializers.ModelSerializer):
+    tasks = serializers.SerializerMethodField('get_task_owner')
+
     class Meta:
         model = User
+        fields = ('id', 'email', 'first_name', 'last_name', 'tasks')
+        write_only_fields = ('password',)
+
+    def get_task_owner(self, obj):
+        return Task.objects.filter(owner=obj).values()
 
 class TaskSerializer(serializers.ModelSerializer):
     owner = UserSerializer(read_only=True)
